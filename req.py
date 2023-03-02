35cad1626099c573f33e780df96c6de4cd83b089
Zolto <72435735+Zzolto@users.noreply.github.com>
2023-03-03 00:48:45 +0700

    Writing bot

--- req.py
+++ req.py
@@ -3,5 +3,10 @@ from aiogram import executor, Dispatcher, Bot, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters import Text
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+TOKEN_API = "sd"
 
+storage = MemoryStorage()
+bot = Bot(TOKEN_API)
+dp = Dispatcher(bot=bot,
+                storage =storage)
 
