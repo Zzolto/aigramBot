0b60ce0dde82b3562452e71818155adb76c80ec6
Zolto <72435735+Zzolto@users.noreply.github.com>
2023-03-03 00:20:12 +0700

    add packages

---
+++ req.py
@@ -0,0 +1,5 @@
+from aiogram import executor, Dispatcher, Bot, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters import Text
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
